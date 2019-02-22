fd836ab379de6123dcb61a2134d59e08e8fd6894
Rob Watts <rob@sherburn.ca>
2019-02-21 22:23:31 -0600

    Changed offset of right sensor
    to 20

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -102,7 +102,7 @@
  * If you have a dual nozzle the offsets are calculated from the primary nozzle (the one plugged in to E0)
  */
 #define SENSOR_LEFT        0
-#define SENSOR_RIGHT       0 // GMF
+#define SENSOR_RIGHT       20 // GMF & RW
 #define SENSOR_FRONT       0 // GMF
 #define SENSOR_BEHIND      0
 
